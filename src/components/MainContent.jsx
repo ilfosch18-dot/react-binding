6af494740c2f86932d3da9c2846a2b61b49bd1d3
ilfosch18-dot <ilfosch18@gmail.com>
2026-09-23 17:16:26 +0200

    first exercise - add reset

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -8,17 +8,20 @@ export default function Main() {
     setFirstCount(firstCount +1);
   }
 
-  
+  function handleAzzeramento(){
+    setFirstCount(0);
+  }
+
   return (
     <main className="text-center">
       <h4>Implemento un contatore numerico incrementabile via bottone 
       e aggiungo un pulsante dedicato per azzerare istantaneamente il valore.
 </h4>
 <section>
-  <p>{firstCount}</p>
+  <p className="fw-bold fs-1 my-5 text-success">{firstCount}</p>
 <button className="btn btn-primary m-3" onClick={handleCounter}>Incrementa</button>
-<button className="btn btn-warning">Azzera</button>
-
+<button className="btn btn-warning" onClick={handleAzzeramento}>Azzera</button>
+<hr class="border border-danger border-2 opacity-50"></hr>
 </section>
 </main>
   )
